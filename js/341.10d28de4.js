ca6f3760134e110933ea7a4ef6ab813d4163a897
AugustoMoheno <augustmoheno@gmail.com>
2024-02-20 04:40:01 -0500

    test demo

---
+++ js/341.10d28de4.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkuviereport"]=self["webpackChunkuviereport"]||[]).push([[341],{1341:function(e,n,t){t.r(n),t.d(n,{default:function(){return a}});var r=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("Demo")])])}],l={name:"DemoView"},c=l,i=t(1001),s=(0,i.Z)(c,r,u,!1,null,null,null),a=s.exports}}]);
+//# sourceMappingURL=341.10d28de4.js.map
